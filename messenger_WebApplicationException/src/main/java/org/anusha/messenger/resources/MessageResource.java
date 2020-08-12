5d7a07060d908c5a65a8f930b5a34deb7952a90e
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-12 22:25:26 +0530

    Add files via upload

---
+++ messenger_WebApplicationException/src/main/java/org/anusha/messenger/resources/MessageResource.java
@@ -0,0 +1,84 @@
+package org.anusha.messenger.resources;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import org.anusha.messenger.model.Message;
+import org.anusha.messenger.resources.beans.MessageFilterBean;
+import org.anusha.messenger.service.MessageService;
+
+@Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class MessageResource {
+
+	MessageService messageService = new MessageService();
+
+	@GET
+	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean)
+
+	{
+		if (filterBean.getYear() > 0) {
+			return messageService.getAllMessagesForYear(filterBean.getYear());
+		}
+		if (filterBean.getStart() >= 0 && filterBean.getSize() > 0) {
+			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+		}
+		return messageService.getAllMessages();
+	}
+
+	@POST
+	public Response addMessages(Message message, @Context UriInfo uriInfo) {
+		Message newMessage = messageService.addMessage(message);
+		String newId = String.valueOf(newMessage.getId());
+		URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
+		return Response.created(uri).entity(newMessage).build();
+
+		// return messageService.addMessage(message);
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+
+	}
+
+	@GET
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId) {
+		return messageService.getMessage(messageId);
+
+	}
+
+	@Path("/{messageId}/comments")
+	public CommentResource getCommentResource() {
+		return new CommentResource();
+	}
+
+}
