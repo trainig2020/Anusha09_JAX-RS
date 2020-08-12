3ed793ea3ae365faa771435ac3b64e84c0bc72da
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-12 15:43:38 +0530

    Add files via upload

---
+++ messenger_Pagination_Filtering/src/main/java/org/anusha/messenger/resources/MessageResource.java
@@ -0,0 +1,68 @@
+package org.anusha.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.anusha.messenger.model.Message;
+import org.anusha.messenger.service.MessageService;
+
+@Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class MessageResource {
+
+	MessageService messageService = new MessageService();
+
+	@GET
+	public List<Message> getMessages(@QueryParam("year") int year, @QueryParam("start") int start,
+			@QueryParam("size") int size)
+
+	{
+		if (year > 0) {
+			return messageService.getAllMessagesForYear(year);
+		}
+		if (start >= 0 && size >= 0) {
+			return messageService.getAllMessagesPaginated(start, size);
+		}
+		return messageService.getAllMessages();
+	}
+
+	@POST
+	public Message addMessages(Message message) {
+
+		return messageService.addMessage(message);
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+
+	}
+
+	@GET
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId) {
+		return messageService.getMessage(messageId);
+
+	}
+
+}
